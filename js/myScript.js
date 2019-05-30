8c614ce50ede940d8828df48fda05e4807e6ed38
JaPut <putans.janis@gmail.com>
2019-05-30 23:13:15 +0300

    FizzBuzzJs

--- js/myScript.js
+++ js/myScript.js
@@ -1,18 +1,17 @@
-
-var i = 1;
+let i = 1;
 do {
-    
     let newDiv = document.createElement("div");
     newDiv.id = Number(i);
-    newDiv.innerHTML = Number(i);
     document.body.appendChild(newDiv);
-    
+      if (i % 15 === 0) {
+        newDiv.innerText = "FizzBuzz";
+      }else if (i % 5 === 0) {
+        newDiv.innerText = "Buzz";
+      }else if (i % 3 === 0) {
+        newDiv.innerText = "Fizz";
+      }else {
+        newDiv.innerText = "Number";
+      }
     i++;
   }
   while (i < 101);
-
- 
-
-
-
-  
