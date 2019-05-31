7d1534ae5223dcc92c6392c208632c51389d108c
JaPut <putans.janis@gmail.com>
2019-05-31 15:11:12 +0300

    Begin

--- js/myScript.js
+++ js/myScript.js
@@ -1,16 +1,19 @@
 let i = 1;
 do {
     let newDiv = document.createElement("div");
-    newDiv.id = Number(i);
     document.body.appendChild(newDiv);
       if (i % 15 === 0) {
         newDiv.innerText = "FizzBuzz";
+        newDiv.id = "fizzBuzz";
       }else if (i % 5 === 0) {
         newDiv.innerText = "Buzz";
+        newDiv.id = "buzz";
       }else if (i % 3 === 0) {
         newDiv.innerText = "Fizz";
+        newDiv.id = "fizz";
       }else {
-        newDiv.innerText = "Number";
+        newDiv.innerText = Number(i);
+        newDiv.id = "number";
       }
     i++;
   }
