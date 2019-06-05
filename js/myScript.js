c20ecc64481ff60c4a547313bf088eb744201c37
JaPut <putans.janis@gmail.com>
2019-06-05 22:18:43 +0300

    Final

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,5 @@
 function theBeginning() {
+  removeElement();
   let i = 1;
     do {
       let newDiv = document.createElement("div");
@@ -25,28 +26,98 @@ function theBeginning() {
     while (i < 101);
   }
 function removeElement() {
-for (i = 1; i < 101; i++) {
-  let myNode = document.getElementById(i);
-       myNode.remove();
+  let main = document.getElementById('main');
+  while (main.firstChild){
+    main.removeChild(main.firstChild);
   }
 }
 function divWillBeBorn() {
   let i = document.getElementById("myInput").value;
-  if (i % 15 == 0) {
-  console.log("BuzzFizz");
-  document.getElementById('main').innerHTML = "I'm buzzfizz";
-  return (i);
-} if (i % 5 == 0) {
+  if (i < 0 || i > 100) {
+    alert("it wasn't asked in homework! :)");
+} if (i % 5 === 0) {
   console.log("Buzz");
-  document.getElementById('main').innerHTML = "I'm buzz";
-} if (i % 3 == 0) {
+  document.getElementById('main').innerHTML = "I'm buzz element with number"+ " " + Number(i);
+} if (i % 3 === 0) {
   console.log("Fizz");
-  document.getElementById('main').innerHTML = "I'm fizz";
-} if (i <0 || i > 100) {
-    alert("No way!");
+  document.getElementById('main').innerHTML = "I'm fizz element with number"+ " " + Number(i);
+} if (i % 15 === 0) {
+  console.log("BuzzFizz");
+  document.getElementById('main').innerHTML = "I'm buzzfizz element with number"+ " " + Number(i);
 } if (i % 15 !== 0 && i % 5 !== 0 && i % 3 !== 0) {
   console.log("Number");
-  document.getElementById('main').innerHTML = "I'm simple number";
+  document.getElementById('main').innerHTML = "However, I'm simple number"+ " " + Number(i);
 }
-}     
+}
+function reset(){
+  var resetButton = document.getElementById("myInput");
+  if(resetButton){
+      resetButton.value= "";
+ }
+}
+function onlyBuzz() {
+  removeElement();
+let i = 1;
+  do {
+    let newDiv = document.createElement("div");
+    document.getElementById("main").appendChild(newDiv);
+        if (i % 5 === 0) {
+        newDiv.innerText = "I'm BUZZ no." + " " + Number(i);
+        newDiv.id = Number(i);
+        newDiv.className = "buzz";
+        }
+        i++;
+     }
+  while (i < 101);
+}
+function onlyFizz() {
+  removeElement();
+  let i = 1;
+    do {
+      let newDiv = document.createElement("div");
+      document.getElementById("main").appendChild(newDiv);
+          if (i % 3 === 0) {
+          newDiv.innerText = "I'm FIZZ no." + " " + Number(i);
+          newDiv.id = Number(i);
+          newDiv.className = "fizz";
+          }
+          i++;
+       }
+    while (i < 101);
+  }
+  let v = 50;
+    function plusOne () {
+      document.getElementById("add").click();
+      v ++;
+      
+      document.getElementById('varValue').innerHTML = v;
+      }
+
+      function minusOne () {
+        document.getElementById("desc").click();
+        v = v - 1;
+        document.getElementById('varValue').innerHTML = v;
+      }
+  function sayMyProperty() {
+        removeElement();
+        let i = v;
+        if (i <0 || i > 100) {
+        alert("it wasn't asked in homework! :)");
+      }if (i === String) {
+        alert("it wasn't asked in homework! :)");
+      }if (i % 15 == 0) {
+        console.log("BuzzFizz");
+        document.getElementById('controlValue').innerHTML = "I'm buzzfizz";
+        return (i);
+      } if (i % 5 == 0) {
+        console.log("Buzz");
+        document.getElementById('controlValue').innerHTML = "I'm buzz";
+      } if (i % 3 == 0) {
+        console.log("Fizz");
+        document.getElementById('controlValue').innerHTML = "I'm fizz";
+      } if (i % 15 !== 0 && i % 5 !== 0 && i % 3 !== 0) {
+        console.log("Number");
+        document.getElementById('controlValue').innerHTML = "I'm simple number";
+      }
+    }
   
